26883d943f57803a2db7a0f49e8a3dcae4146adb
Ty Bukowski <shuteman2@aol.com>
2019-03-09 11:28:50 -0600

    added readme

---
+++ README.md
@@ -0,0 +1,13 @@
+# First WordPress Theme
+
+A short bit about the project.
+
+## Future work
+
+- Convert front-page.php to a home page page template
+- Create home page that uses home page page template
+- Create testimonials post type
+- On home page template, query for and display testimonials post type in testimonials section
+- Create 3 widget areas on home page page template and widget
+- Load featured image, if set, on home page page template
+- Echo the content in bottom left if there is content
---
+++ front-page.php
@@ -0,0 +1,48 @@
+<?php get_header();?>
+<img class="top_pic" src="./consulting/main_img.png" alt="snowboarder">
+                <div class="outside_block"> <!--wrench color-->
+                    <img class="wrench one" src="./consulting/icon3.png" alt="wrench">
+                    <h2>SERVICES</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut a
+                        mauris id lorem ullamcorper imperdiet. Praesent sollicitudin
+                        nulla semper massa feugiat mattis. Sed aliquam orci
+                        dolor, sit amet fermentum purus congue non...</p>
+                    <button>More Info</button>
+                </div>
+                <div class="inside_block">
+                    <img class="wrench two" src="./consulting/icon3.png" alt="wrench">
+                    <h2>PROJECTS</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut a
+                        mauris id lorem ullamcorper imperdiet. Praesent sollicitudin
+                        nulla semper massa feugiat mattis. Sed aliquam orci
+                        dolor, sit amet fermentum purus congue non...</p>
+                    <button>More Info</button>
+                </div>
+                <div class="outside_block">
+                    <img class="wrench three" src="./consulting/icon3.png" alt="wrench">
+                    <h2>CLIENTS</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut a
+                        mauris id lorem ullamcorper imperdiet. Praesent sollicitudin
+                        nulla semper massa feugiat mattis. Sed aliquam orci
+                        dolor, sit amet fermentum purus congue non...</p>
+                    <button>More Info</button>
+                </div>
+                <div class="goal">
+                    <h2>THE WORLD CAN BE YOURS!</h2>
+                    <img src="./consulting/building.png" alt="building">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut a
+                        mauris id lorem ullamcorper imperdiet.<br>
+                    <br>Sollicitudin nulla semper massa feugiat mattis. Lorem ipsum 
+                        dolor sit amet, consectetur adipiscing elit. Ut a
+                        mauris id lorem ullamcorper imperdiet.<br>
+                    <br>Praesent sollicitudin nulla semper massa feugiat mattis.<br><br>
+                    <input type="submit" value="Learn more" id="goal_button">
+                </div>
+                <div class="testimonial">
+                    <h2>TESTIMONIAL</h2>
+                    <div class="arrow"></div>
+                    <p>"I'm a testimonial. Click me to edit and add text that says 
+                        something nice about you and your services."</p>
+                </div>
+                <p class="manager">Samantha Jones, Project Manager</p>
+<?php get_footer();?>
