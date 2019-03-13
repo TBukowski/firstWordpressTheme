a2f7ce2027d289efd195ff16ef73bd6aac89b172
Ty Bukowski <shuteman2@aol.com>
2019-03-12 22:43:36 -0500

    adjusted height for .wrapper & .content.. cleaned up functions.php

--- functions.php
+++ functions.php
@@ -1,21 +1,22 @@
 <?php
 
 // https://developer.wordpress.org/reference/functions/wp_register_style/
+// wp_enqueue_style( $handle, $src, $deps, $ver, $media );
 function load_stylesheets()
 {
-    wp_register_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), false, 'all');
-    wp_enqueue_style('bootstrap');
+    // wp_register_style('bootstrap');
+    wp_enqueue_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), false, 'all');
 
-    wp_register_style('stylesheet', get_template_directory_uri() . '/style.css', array(), false, 'all');
-    wp_enqueue_style('stylesheet');
+    // wp_register_style('stylesheet');
+    wp_enqueue_style('stylesheet', get_template_directory_uri() . '/style.css', array(), false, 'all');
 }
 add_action('wp_enqueue_scripts', 'load_stylesheets');
 
 
 // https://developer.wordpress.org/reference/functions/wp_register_script/
+// wp_enqueue_script( $handle, $src, $deps, $ver, $in_footer);
 function load_js()
 {
-
     wp_enqueue_script('scripts', get_template_directory_uri() . '/js/scripts.js', array( 'jquery' ), 1, true);
 }
 add_action('wp_enqueue_scripts', 'load_js');
