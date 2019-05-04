948e5ec0cf52d60f09ff73bf1f02f580ff087e3b
Ty Bukowski <shuteman2@aol.com>
2019-05-04 00:45:50 -0500

    worked on converting more of the page to flexbox. minor styling tweaks still needed

--- home-template.php
+++ home-template.php
@@ -32,35 +32,34 @@ get_header();
                     <?php endif; ?>
 
                 </div>
-                <div class="goal">
-                    <h2>THE WORLD CAN BE YOURS!</h2>
-                    <img src="<?php echo get_template_directory_uri(); ?>./consulting/building.png" alt="building">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut a
-                        mauris id lorem ullamcorper imperdiet.<br>
-                    <br>Sollicitudin nulla semper massa feugiat mattis. Lorem ipsum 
-                        dolor sit amet, consectetur adipiscing elit. Ut a
-                        mauris id lorem ullamcorper imperdiet.<br>
-                    <br>Praesent sollicitudin nulla semper massa feugiat mattis.<br><br>
-                    <input type="submit" value="Learn more" id="goal_button">
+                <div class="bottom_content">
+                    <div class="goal">
+                        <div class="headline_pic">
+                            <h2>THE WORLD CAN BE YOURS!</h2>
+                            <img src="<?php echo get_template_directory_uri(); ?>./consulting/building.png" alt="building">
+                        </div>
+                        <div class="goal_text">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut a mauris id lorem ullamcorper imperdiet. 
+                                Sollicitudin nulla semper massa feugiat mattis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                                Ut a mauris id lorem ullamcorper imperdiet. Praesent sollicitudin nulla semper massa feugiat mattis.</p>
+                            <input type="submit" value="Learn more" id="goal_button">
+                        </div>
+                    </div>
+                    <div class="testimonial">
+                        <h2>TESTIMONIAL</h2>
+                        <div class="arrow"></div>
+                        <?php
+                            $args = array( 
+                                'post_type' => 'testimonies', 
+                                'orderby'   => 'rand',
+                                'posts_per_page' => 1 );
+                            $loop = new WP_Query( $args );
+                                while ( $loop->have_posts() ) : $loop->the_post();
+                                    echo '<div class="entry-content">';
+                                    the_content();
+                                    echo '</div>';
+                                endwhile
+                        ?>        
+                    </div>
                 </div>
-                <div class="testimonial">
-                    <h2>TESTIMONIAL</h2>
-                    <div class="arrow"></div>
-                    <?php
-                        $args = array( 
-                            'post_type' => 'testimonies', 
-                            'orderby'   => 'rand',
-                            'posts_per_page' => 1 );
-                        $loop = new WP_Query( $args );
-                            while ( $loop->have_posts() ) : $loop->the_post();
-                                echo '<div class="entry-content">';
-                                the_content();
-                                echo '</div>';
-                            endwhile
-                    ?>        
-                        <!-- <p>"I'm a testimonial. Click me to edit and add text that says 
-                            something nice about you and your services."</p> -->
-                </div>
-                <!-- <p class="manager">Samantha Jones, Project Manager</p> -->
-
 <?php get_footer();?>
