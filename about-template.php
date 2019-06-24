3deb97d608773ce918179ccb9ccfe465e6d73456
Ty Bukowski <shuteman2@aol.com>
2019-06-23 22:20:38 -0500

    moved about page to its own template, added css classes to project blocks

--- about-template.php
+++ about-template.php
@@ -1,36 +1,17 @@
-<?php 
-
+<?php
 /* 
     Template Name: About Page Template
 */
 
-get_header();?>
-
-    <?php if (has_post_thumbnail()) : ?>
-        <img class="top_pic_about" src="<?php echo wp_get_attachment_url( get_post_thumbnail_id() ); ?>" alt="building">
-    <?php endif; ?>
-
-    <?php
-
-    if ( have_posts() ) : while ( have_posts() ) : the_post(); 
-
-        the_content();
-
-        endwhile;
-
-    endif; ?>
-
-    <?php
-        $args = array(
-            'post_type' => 'about',
-            'posts_per_page' => 1 );
-        $loop = new WP_Query( $args );
-            while ( $loop->have_posts() ) : $loop->the_post();
-                echo '<div class="about_text">';
-                the_content();
-                echo '</div>';
-            endwhile
-    ?>
-
-
+ get_header();?>
+    <div class="index-content">
+        <?php if (have_posts()) : ?>
+            <?php while (have_posts()) : the_post(); ?>
+                <div <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+                    <!-- <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1> -->
+                    <?php the_content(); ?>
+                </div>
+            <?php endwhile; ?>
+        <?php endif; ?>
+    </div>
 <?php get_footer();?>
--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php get_header();?>
-    <?php if (have_posts()) : ?>
-        <?php while (have_posts()) : the_post(); ?>
-            <div <?php post_class(); ?> id="post-<?php the_ID(); ?>">
-                <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-                <?php the_content(); ?>
-            </div>
-        <?php endwhile; ?>
-    <?php endif; ?>
+    <div class="index-content">
+        <?php if (have_posts()) : ?>
+            <?php while (have_posts()) : the_post(); ?>
+                <div <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+                    <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+                    <?php the_content(); ?>
+                </div>
+            <?php endwhile; ?>
+        <?php endif; ?>
+    </div>
 <?php get_footer();?>
