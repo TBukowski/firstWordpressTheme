7e55a7d55129043840171e0f1b90c8c720747376
Ty Bukowski <shuteman2@aol.com>
2019-06-29 11:13:46 -0500

    added to the styling of the pages

--- about-template.php
+++ about-template.php
@@ -8,7 +8,7 @@
         <?php if (have_posts()) : ?>
             <?php while (have_posts()) : the_post(); ?>
                 <div <?php post_class(); ?> id="post-<?php the_ID(); ?>">
-                    <!-- <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1> -->
+                    <h1 class="page-heading"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
                     <?php the_content(); ?>
                 </div>
             <?php endwhile; ?>
--- index.php
+++ index.php
@@ -3,8 +3,10 @@
         <?php if (have_posts()) : ?>
             <?php while (have_posts()) : the_post(); ?>
                 <div <?php post_class(); ?> id="post-<?php the_ID(); ?>">
-                    <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-                    <?php the_content(); ?>
+                    <div class="content-area">
+                        <h1 class="page-heading"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+                        <?php the_content(); ?>
+                    </div>
                 </div>
             <?php endwhile; ?>
         <?php endif; ?>
