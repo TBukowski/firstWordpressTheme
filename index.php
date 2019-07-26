1ff93dce31f7440daf152eec7f26a027fbad125b
Ty Bukowski <shuteman2@aol.com>
2019-07-25 21:43:27 -0500

    moved sidebar to right side of default template

--- index.php
+++ index.php
@@ -1,19 +1,23 @@
 <?php get_header();?>
     <div class="index-content">
-        <?php if (have_posts()) : ?>
-            <?php while (have_posts()) : the_post(); ?>
-                <div <?php post_class(); ?> id="post-<?php the_ID(); ?>">
-                    <div class="content-area">
-                        <h1 class="page-heading"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-                        <?php the_content(); ?>
-                    </div>
-                    <?php if (is_active_sidebar( 'index_sidebar' )) : ?>
-                        <div>
-                            <?php dynamic_sidebar( 'index_sidebar' ); ?>
+        <div class="index-main">
+            <?php if (have_posts()) : ?>
+                <?php while (have_posts()) : the_post(); ?>
+                    <div <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+                        <div class="content-area">
+                            <h1 class="page-heading"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+                            <?php the_content(); ?>
                         </div>
-                    <?php endif; ?>
+                    </div>
+                <?php endwhile; ?>
+            <?php endif; ?>
+        </div>
+        <div class="index-side">
+            <?php if (is_active_sidebar( 'index_sidebar' )) : ?>
+                <div>
+                    <?php dynamic_sidebar( 'index_sidebar' ); ?>
                 </div>
-            <?php endwhile; ?>
-        <?php endif; ?>
+            <?php endif; ?>
+        </div>
     </div>
 <?php get_footer();?>
